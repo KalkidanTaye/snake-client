5f4b9907560606dd9996c6612855e6264fabc864
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-26 05:23:04 -0400

    add movement keys

--- input.js
+++ input.js
@@ -3,6 +3,18 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
   }
+  if (key === "w") {
+    connection.write("Move up");
+  }
+  if (key === "a") {
+    connection.write("Move left");
+  }
+  if (key === "s") {
+    connection.write("Move down");
+  }
+  if (key === "d") {
+    connection.write("Move right");
+  }
 };
 
 const setupInput = function (conn) {
