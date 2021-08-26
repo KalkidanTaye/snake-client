d861552c6aa64510c307c5159066ba968ec7e9be
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-26 04:45:17 -0400

    add setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,7 +1,23 @@
 const { connect } = require("./client");
 
 console.log("Connecting ...");
+
+const handleUserInput = function () {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 // const conn = connect();
 // setupInput(conn);
-
+setupInput();
 connect();
